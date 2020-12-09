f541c293545e7e3835616243d2bd7d79568e5b30
Albert Kolozsvari <albertartk@gmail.com>
2020-12-09 18:04:48 +0000

    Add notice create/update/delete endpoint

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,18 @@
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from canonicalwebteam.flask_base.app import FlaskBase
+from flask import jsonify, make_response
 
-from webapp.views import get_cve, get_notice, get_cves, get_notices
 from webapp.api_spec import WebappFlaskApiSpec
+from webapp.views import (
+    get_cve,
+    get_notice,
+    get_cves,
+    get_notices,
+    create_notice,
+    update_notice,
+    delete_notice,
+)
 
 app = FlaskBase(
     __name__,
@@ -47,8 +56,42 @@ app.add_url_rule(
     provide_automatic_options=False,
 )
 
+app.add_url_rule(
+    "/security/notices",
+    view_func=create_notice,
+    methods=["POST"],
+    provide_automatic_options=False,
+)
+
+app.add_url_rule(
+    "/security/notices/<notice_id>",
+    view_func=update_notice,
+    methods=["PUT"],
+    provide_automatic_options=False,
+)
+
+app.add_url_rule(
+    "/security/notices/<notice_id>",
+    view_func=delete_notice,
+    methods=["DELETE"],
+    provide_automatic_options=False,
+)
+
 docs = WebappFlaskApiSpec(app)
 docs.register(get_cve)
 docs.register(get_cves)
 docs.register(get_notice)
 docs.register(get_notices)
+docs.register(create_notice)
+docs.register(update_notice)
+docs.register(delete_notice)
+
+
+@app.errorhandler(422)
+def handle_error(error):
+    messages = error.data.get("messages")
+
+    return make_response(
+        jsonify({"message": "Invalid payload", "errors": messages["json"]}),
+        422,
+    )
