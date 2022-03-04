acff59db901ffe81cd0135f09eb9253202b04166
Robin Winslow <robin@robinwinslow.co.uk>
2022-03-04 15:55:09 +0000

    Add create scripts for creating through API

---
+++ scripts/create-notice.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+# Standard library
+import argparse
+import json
+import os
+from datetime import datetime
+from http.cookiejar import MozillaCookieJar
+
+# Packages
+from macaroonbakery import httpbakery
+
+
+parser = argparse.ArgumentParser(description="Create security notices through the local API")
+parser.add_argument("file_path", action="store", type=str)
+parser.add_argument(
+    "--host", action="store", type=str, default="http://localhost:8030"
+)
+args = parser.parse_args()
+
+
+client = httpbakery.Client(cookies=MozillaCookieJar(".login"))
+
+if os.path.exists(client.cookies.filename):
+    client.cookies.load(ignore_discard=True)
+
+notice_endpoint = f"{args.host}/security/notices.json"
+
+# Make a first call to make sure we are logged in
+response = client.request("POST", url=notice_endpoint)
+client.cookies.save(ignore_discard=True)
+
+# Post the stuff
+with open(args.file_path) as json_file:
+    response = client.request(
+        "POST",
+        url=notice_endpoint,
+        json=json.load(json_file),
+    )
+
+print(response, response.text)
