15c0633b377fca22f62eb1e2e1a349b7d2a21c90
Olwe Samuel <samuel.olwe@canonical.com>
2025-09-09 13:35:13 +0300

    Set connection pool recycle time to 3600 seconds

--- webapp/database.py
+++ webapp/database.py
@@ -31,7 +31,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy  # noqa: E402
 from sqlalchemy import exc
 
-db = SQLAlchemy(session_options={"autoflush": False}, engine_options={"pool_pre_ping": True})
+db = SQLAlchemy(session_options={"autoflush": False}, engine_options={"pool_pre_ping": True, "pool_recycle": 3600})
 
 
 def init_db(app):
